6895fc144b93b028dd8c9acbfe1fab7e1b977786
Rodion Gibadullin <rodion-stalker@mail.ru>
2022-08-19 22:15:11 +0300

    Решение третьей задачи/решение с одногруппниками в телеграме

--- Program.cs
+++ Program.cs
@@ -36,3 +36,20 @@ int SumNumber(int number)
 int sumNumber = SumNumber(number);
 Console.WriteLine("Сумма цифр в данном числе: " + sumNumber);
 
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int [] numbers = new int[8];
+Console.Write("[");
+
+for (int i = 0; i < numbers.Length; i++)
+ {
+    numbers [i] = new Random().Next(0, 11);
+    Console.Write(" " + Method (i) + " ");
+ }
+Console.Write("]");
+
+int Method (int a)
+{
+    return numbers[a];
+}
+
