7cf40145987380427c0666bc39261db6f3ef023b
Rodion Gibadullin <rodion-stalker@mail.ru>
2022-08-19 22:04:26 +0300

    Решение второй задачи

--- Program.cs
+++ Program.cs
@@ -10,3 +10,29 @@ for (int i = 1; i < numberB; i++)
 step = step * numberA;
 }
 Console.WriteLine("A в степени B равно: " + step);
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. 
+
+
+Console.WriteLine("Введите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int SumNumber(int number)
+  {
+    
+    int counter = Convert.ToString(number).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++)
+    {
+      advance = number - number % 10;
+      result = result + (number - advance);
+      number = number / 10;
+    }
+   return result;
+  }
+
+int sumNumber = SumNumber(number);
+Console.WriteLine("Сумма цифр в данном числе: " + sumNumber);
+
